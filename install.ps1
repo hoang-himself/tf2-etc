6661cfae5e087678fc7d9b5693ec1a083389eec6
Hoang NGUYEN <baauco@gmail.com>
2023-08-10 10:21:29 +0700

    feat: accept and test path as param

--- install.ps1
+++ install.ps1
@@ -1,4 +1,10 @@
-$Path = 'D:\SteamLibrary\steamapps\common\Team Fortress 2\tf'
+param(
+  [string]$Path = 'D:\SteamLibrary\steamapps\common\Team Fortress 2\tf'
+)
+
+if (-not (Test-Path -Path "$Path" -PathType Container)) {
+  throw "Invalid path: $Path"
+}
 
 Get-ChildItem -Directory -Path 'custom' | ForEach-Object -Process {
   New-Item -ItemType SymbolicLink -Path "$Path\custom\$($_.Name)" -Target $_.FullName -Force
